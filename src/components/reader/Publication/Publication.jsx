ffef79baf6760eb86d97f5e0fabc92314c6dd1f6
Artem2313 <huselnykov@gmail.com>
2020-01-03 12:48:36 +0200

    some_styles

--- src/components/reader/Publication/Publication.jsx
+++ src/components/reader/Publication/Publication.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Styles.module.css';
 
 const Publication = ({ publication: { title, text } }) => {
   return (
     <article>
-      <h2>{title}</h2>
-      <p>{text}</p>
+      <h2 styles={styles}>{title}</h2>
+      <p styles={styles}>{text}</p>
     </article>
   );
 };
